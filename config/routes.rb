eee77468ea7e171e1a1a3d6718e0a03e6419ecfc
koki-73 <kokisasaki.htnh@gmail.com>
2020-04-29 16:25:32 +0900

    prepare necessary files
    ・コントローラ作成
    ・ビューファイル作成
    ・ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :items, only: :show
   resources :categories, only: :show
   resources :brands, only: :index
+  resources :credit_cards, only: [:index, :new, :create, :destroy]
 end
