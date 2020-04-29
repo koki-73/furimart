e08f16be1fd9ad2461f61ccc721ce5bc32e28728
kana1203 <t.kana1203@gmail.com>
2020-04-29 14:53:03 +0900

    gem-factory_bot_rails-install&user-test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,89 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+    it "全ての項目がある場合は登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
     it "nicknameがない場合は登録できないこと" do
-      user = User.new(nickname: "", email: "test@gmeil.com", password: "00000000", password_confirmation: "00000000", family_name: "高橋", first_name: "奏", family_name_kana: "タカハシ", first_name_kana: "カナ", birth_day: "1992-12-03")
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
+
+    it "emailがない場合は登録できないこと" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it " 重複したemailが存在する場合は登録できないこと " do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+
+    it "emailに@が含まれていない場合は登録できないこと" do
+      user = build(:user, email: "testtest")
+      user.valid?
+      expect(user.errors[:email]).to include("is invalid")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it " passwordが7文字以上であれば登録できること " do
+      user = build(:user, password: "0000000", password_confirmation: "0000000")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    it " passwordが6文字以下であれば登録できないこと " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 7 characters)")
+    end
+    
+    it "family_nameがない場合は登録できないこと" do
+      user = build(:user, family_name: "")
+      user.valid?
+      expect(user.errors[:family_name]).to include("can't be blank")
+    end
+
+    it "first_nameがない場合は登録できないこと" do
+      user = build(:user, first_name: "")
+      user.valid?
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+
+    it "family_name_kanaがない場合は登録できないこと" do
+      user = build(:user, family_name_kana: "")
+      user.valid?
+      expect(user.errors[:family_name_kana]).to include("can't be blank")
+    end
+
+    it "first_name_kanaがない場合は登録できないこと" do
+      user = build(:user, first_name_kana: "")
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("can't be blank")
+    end
+
+    it "birth_dayがない場合は登録できないこと" do
+      user = build(:user, birth_day: "")
+      user.valid?
+      expect(user.errors[:birth_day]).to include("can't be blank")
+    end
+
   end
 end
