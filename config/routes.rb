ab538af3f622490ec84abe2b71c276cabb45a6b1
koki-73 <kokisasaki.htnh@gmail.com>
2020-04-30 10:45:44 +0900

    credit card register function
    ・jquery導入
    ・コントローラの記述
    ・登録、削除用の画面
    ・ルーティング設定
    ・マイページのメニュー部分テンプレート化

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
   resources :items, only: :show
   resources :categories, only: :show
   resources :brands, only: :index
-  resources :credit_cards, only: [:index, :new, :create, :destroy]
+  resources :credit_cards, only: [:index, :new, :show] do
+    collection do
+      post 'show', to: 'credit_cards#show'
+      post 'pay', to: 'credit_cards#pay'
+      post 'delete', to: 'credit_cards#delete'
+    end
+  end
 end
