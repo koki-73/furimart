00eb5c7f130e5e7d4af19bb1982cb921ae9f9980
keizoukatagiri <kotetu51021996@gmail.com>
2020-05-02 09:28:52 +0900

    商品出品1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,44 @@ class ItemsController < ApplicationController
 
   end
 
+  def new
+    @item = Item.new   
+    @category_parent_array = Category.where(ancestry: nil)
+
+    # Category.where(ancestry: nil).each do |parent|
+    #   @category_parent_array << parent.name
+    # end
+
+    # @category_parent_array_name = []
+    # @category_parent_array_id = []
+    # Category.where(ancestry: nil).each do |parent|
+    #   @category_parent_array_name << parent.name
+    #   @category_parent_array_id << parent.id
+    # end
+  end
+
+  def get_category_children
+   
+    @category_children = Category.find(params[:parent_id]).children
+  end
+
+  def get_category_grandchildren
+    child_id = child_params[:child_id]
+    @category_grandchildren = Category.find(child_id).children
+    
+  end
+
+  def create
+    Item.create
+  end
+
   def show
   end
+  private
+  def child_params
+    params.permit(:child_id)
+  end
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
+  root 'items#new'
+  # root 'items#index'
+  resources :items, only: [:index, :show, :new, :edit, :destroy] do
+    #Ajaxで動くアクションのルートを作成
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
   resources :users, only: :show
   resources :my_pages, only: :index
-  resources :items, only: :show
+  # resources :items, only: [:show, :new, :create] 
   resources :categories, only: :show
   resources :brands, only: :index
 end
