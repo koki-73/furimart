600a785173d7af7a6f22f40296d73f5b8889a2df
keizoukatagiri <kotetu51021996@gmail.com>
2020-05-04 12:05:02 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#new'
-
+  root 'items#index'
+  resources :items, only: [:index, :show, :new, :create, :edit, :destroy] do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
   resources :users, only: :show
   resources :my_pages, only: :index
   resources :items, only: [:show, :new, :create]
