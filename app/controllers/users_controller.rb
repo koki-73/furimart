459dc0be20484cd3a9d524af752293e1dbf7e8ac
koki-73 <kokisasaki.htnh@gmail.com>
2020-05-05 14:29:22 +0900

    commit for rebase
    ・リベースのために一時コミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @items = Item.where(user_id: @user.id).includes(:item_images)
+    # binding.pry
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_001403) do
+
+ActiveRecord::Schema.define(version: 2020_05_01_003005) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
