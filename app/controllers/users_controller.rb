1b0f50fc62634be35cfe42b157d57e660ab062da
koki-73 <kokisasaki.htnh@gmail.com>
2020-05-05 15:46:38 +0900

    Adjust code
    ・いいねの表示を場合分け
    ・不要なコードの整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @items = Item.where(user_id: @user.id).includes(:item_images)
-    # binding.pry
   end
 end
