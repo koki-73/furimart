c73eaf7957b170723a8a58967b79c83d6d764e73
sidiauracid <miya.programming@gmail.com>
2020-05-06 16:27:01 +0900

    image-uploader-form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,8 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new   
+    @item = Item.new
+    @item.item_images.new
     @category_parent_array = Category.where(ancestry: nil)
   end
   def get_category_children
@@ -13,6 +14,17 @@ class ItemsController < ApplicationController
   def get_category_grandchildren
     child_id = child_params[:child_id]
     @category_grandchildren = Category.find(child_id).children 
+  end
+
+  def create
+    # binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      
+      redirect_to new_item_path
+    end
   end
 
   def show
@@ -25,4 +37,9 @@ class ItemsController < ApplicationController
   def child_params
     params.permit(:child_id)
   end
+  
+  def item_params
+    params.require(:item).permit(:name, :price, item_images_attributes: [:image])
+  end
+
 end
