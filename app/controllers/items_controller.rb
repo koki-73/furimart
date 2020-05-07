4bf9529d92642f2efd1d94b12581780cccfae2a5
kana1203 <t.kana1203@gmail.com>
2020-05-07 22:43:47 +0900

    fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.includes(:item_images)
-    # @items_brand = Item.where(brand: "test").includes(:item_images).offset(0).limit(3)
+    @items = Item.includes(:item_images).offset(0).limit(3)
+    @items_brand = Item.where(brand: "nike").includes(:item_images).offset(0).limit(3)
   end
 
   def new
