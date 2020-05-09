5288326589fffad43176a59de154628160f6ac8d
itiwata <wara1.taku3@i.softbank.jp>
2020-05-09 17:50:22 +0900

    retake2

--- app/assets/javascripts/channels/owl-carousel.js
+++ app/assets/javascripts/channels/owl-carousel.js
@@ -10,8 +10,7 @@ $(function(){
     $(".owl__carousel__main").removeClass("show").eq(index).addClass("show")
   };
   function imageNotSelect(){
-    $('.active').css('opacity', '1');
-    $(".owl__carousel__sub").css('opacity', '0.4');
+    $(".active").css('opacity', '0.4');
   }
   mouses.mouseenter(imageSelect); 
   mouses.mouseleave(imageNotSelect)
