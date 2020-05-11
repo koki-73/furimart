5ea80adb73d70a40919e967b251af9a441048fd2
koki-73 <kokisasaki.htnh@gmail.com>
2020-05-11 18:21:07 +0900

    revise comment-out

--- app/assets/javascripts/items-edit.js
+++ app/assets/javascripts/items-edit.js
@@ -28,32 +28,26 @@ $(function(){
   if (fileIndex.length == 0) $("#image-wrapper").css("display","none");
 
   $("#image-box").on("change", function(e){
-    // プレビューの画像枚数とフォームの数をカウント
     let imageCount = $(".new-wrapper__main__preview__image").length
     let formCount = $(".js-file-group").length
-
     const blobUrl = window.URL.createObjectURL(e.target.files[0]);
     $("#previews").append(buildImg(fileIndex[0], blobUrl));
     $(".new-wrapper__main__image-field").append(buildFileField(formCount));
     $(".js-file-group").css("display","none");
     $("#image-wrapper").attr("for",`item_item_images_attributes_${formCount}_image`)
     fileIndex.shift();
-    // プレビューが１０枚だったら入力欄を非表示
+
     if (imageCount == 9) $("#image-wrapper").css("display","none");
   })
 
   $('#image-box').on('click', '.delete-icon', function() {
-    // 画像入力欄を表示する
     $("#image-wrapper").css("display","block");
-    // データのインデックスを特定する
     const targetIndex = $(this).data('index')
-    // 該当indexを振られているチェックボックスを取得する
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
     fileIndex.unshift(10 - fileIndex.length);
-    // もしチェックボックスが存在すればチェックを入れる
+
     if (targetIndex < fileCount) {
       hiddenCheck.prop('checked', true)
-
       $("#image-wrapper").attr("for",`item_item_images_attributes_${fileCount}_image`);
     } else {
       $(".js-file-group")[(targetIndex)].remove();
@@ -62,11 +56,10 @@ $(function(){
       $("#image-wrapper").attr("for",`item_item_images_attributes_${fileIndex[0] - 1}_image`);
     };
     
-    // プレビューの画像を削除
     $(this).parent().parent().remove();
     $(`img[data-index="${targetIndex}"]`).remove();
     
-    // 画像入力欄が0個にならないようにしておく
+
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
     
   });
