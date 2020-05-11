bb3803e3f82bce7bdf761dda0c87eecf1b09a901
itiwata <wara1.taku3@i.softbank.jp>
2020-05-11 11:00:47 +0900

    retake1

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_item, only: [:create, :destroy]
   def create
-    @like = current_user.likes.create(item_id: params[:item_id])
+    @item(user_id) = current_user.id
+    @like = current_user.likes.create(params[:item_id])
+    redirect_to items_path(@item)
   end
 
   def destory
     @like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
-    @like.destroy
+    @like.destory
+    redirect_to items_path(@item)
   end
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
