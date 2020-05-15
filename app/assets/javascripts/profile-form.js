308035526618e2de976b89bc400d72659f6b3547
koki-73 <kokisasaki.htnh@gmail.com>
2020-05-15 18:17:22 +0900

    add function profile-image register
    ・プロフィール登録フォームに画像を追加
    ・マイページ、ユーザー詳細ページにプロフ画像を表示

---
+++ app/assets/javascripts/profile-form.js
@@ -0,0 +1,18 @@
+$(function(){
+  const buildImg = (url)=> {
+    const html = `<div class="profile-image">
+                  <img width="150px" height="150px" src="${url}" >
+                  </div>`;
+    return html;
+  }
+
+  // $("#profile-image-preview").attr("for",`profile[image]`);
+  
+  $("#profile-image-form").on("change", function(e){
+    console.log("OK")
+    $("img").remove();
+    const blobUrl = window.URL.createObjectURL(e.target.files[0]);
+    $("#profile-image-preview").append(buildImg(blobUrl));
+    $("img").css("border-radius","50%");
+  })
+})
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @profile = Profile.find_by(user_id: @user.id)
     @items = Item.where(user_id: @user.id).includes(:item_images)
   end
 end
